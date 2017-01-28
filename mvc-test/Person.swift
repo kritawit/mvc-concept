1fa224032eed8113c53922e796a4d983258e8e33
Header-Develop <kritawitbunket@gmail.com>
2017-01-29 00:10:14 +0700

    Project for Lean MVC concept

---
+++ mvc-test/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  mvc-test
+//
+//  Created by Header-Develop on 1/28/2560 BE.
+//  Copyright © 2560 Header-Development. All rights reserved.
+//
+
+import Foundation
---
+++ mvc-test/RoundedImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedImageView.swift
+//  mvc-test
+//
+//  Created by Header-Develop on 1/28/2560 BE.
+//  Copyright © 2560 Header-Development. All rights reserved.
+//
+
+import UIKit
+
+class RoundedImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
